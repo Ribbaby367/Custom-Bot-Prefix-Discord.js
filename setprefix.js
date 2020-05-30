a90ad947b93b28d0648356531f0d024878b4a59a
Ghostoblivion <HeathH-M@programmer.net>
2020-05-31 10:20:17 +1200

    Update setprefix.js
    I'm retarded and forgot to change RichEmbed to MessageEmbed.

--- setprefix.js
+++ setprefix.js
@@ -3,7 +3,7 @@ exports.run = async (client, Discord, message, args, db, config) => {
   let prefix = await db.fetch(`prefix_${message.guild.id}`);
   if (args[0] === undefined) return message.channel.send(`No prefix was specified! Please add a prefix.`);
   db.set(`prefix_${message.guild.id}`, args[0]);
-    let embed = new Discord.RichEmbed()
+    let embed = new Discord.MessageEmbed()
     .setDescription(`${message.author} successfully set the servers prefix to ${args[0]}!`)
     .setColor("#55ce58")
     .setTimestamp()
